da84b8db81f8539def590264ab344d7822316b3f
gabyfulchic <gaby.fulchic@ynov.com>
2019-07-01 15:23:55 +0200

    added numberOfFiles

--- log-turn-on.sh
+++ log-turn-on.sh
@@ -1,15 +1,26 @@
-#!/bin/zsh
+#!/bin/bash
+
+create_backupfile () {
+    cd /home/easylia/turn-on-log
+    touch "turn-on-log.$(date +%F_%R).log"
+    sudo chmod 777 "turn-on-log.$(date +%F_%R).log"
+    echo "Le pc a démarré automatiquement le : $(date)" >> /home/easylia/turn-on-log/turn-on-log.$(date +%F_%R).log
+}
 
 #creating the folder and verifying if it already exists
 sudo mkdir /home/easylia/turn-on-log
 if [ $? -eq 0 ]; then
-	sudo chmod 777 /home/easylia/turn-on-log
+    sudo chmod 777 /home/easylia/turn-on-log
 fi
 
-cd /home/easylia/turn-on-log
+#count number of files in the folder
+numberOfFiles="$(ls -1 | wc -l)"
 
-touch "turn-on-log.$(date +%F_%R).log"
-
-sudo chmod 777 "turn-on-log.$(date +%F_%R).log"
-
-echo "Le pc a démarré automatiquement le : $(date)" >> /home/easylia/turn-on-log/turn-on-log.$(date +%F_%R).log
+if [ $numberOfFiles -ge "30" ]
+then
+    rm "$(ls -t | tail -1)"
+    create_backupfile
+elif [ $numberOfFiles -lt "30" ]
+then
+    create_backupfile
+fi
