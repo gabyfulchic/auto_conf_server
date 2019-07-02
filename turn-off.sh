973254508ab15ab871af7d1985faf6e009e38664
gabyf <gaby.fulchic@ynov.com>
2019-07-02 11:05:56 +0200

    Update the script

--- turn-off.sh
+++ turn-off.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-#sudo to by-pass password
-#log all shutdowns in /home/easylia
-mkdir /home/easylia/shutdown-logs
-cd /home/easylia/shutdown-logs
-sudo touch "turn-off.$(date +%F_%R).log"
+# log all shutdowns in /home/easylia
+logPlace="/home/easylia/shutdown-logs"
+numberOfFiles="$(ls -1 | wc -l)"
 
-# read/write/execute only for easylia and read-only for other
-sudo chmod 777 /home/easylia/shutdown-logs/turn-off.$(date +%F_%R).log
+mkdir $logPlace
+cd $logPlace
 
-#launch the shutdown called by the cron if the cho well worked
+if [ $numberOfFiles -ge "30" ]; then
+    sudo rm "$(ls -t | tail -1)"
+    sudo touch "turn-off.$(date +%F_%R).log"
+elif [ $numberOfFiles -lt "30" ]; then
+    sudo touch "turn-off.$(date +%F_%R).log"
+fi
+
+# read/write/execute only for easylia and read/exec for other
+sudo chmod 755 $logPlace/turn-off.$(date +%F_%R).log
+
+# launch the basic log if no args are given to the script
 if [ $# -eq 0 ]; then
-	sudo echo "Shutdown occured at : $(date) " >> /home/easylia/shutdown-logs/turn-off.$(date +%F_%R).log
-	sudo shutdown -h now
+    sudo echo "Shutdown occured at : $(date) " >> $logPlace/turn-off.$(date +%F_%R).log
+    sudo shutdown -h now
 else
-	sudo echo "raison du shutdown : $1 $2 $3" >> /home/easylia/shutdown-logs/turn-off.$(date +%F_%R).log
-	sudo shutdown -h now
+    sudo echo "Raison du shutdown : $1 $2 $3" >> $logPlace/turn-off.$(date +%F_%R).log
+    sudo shutdown -h now
 fi
