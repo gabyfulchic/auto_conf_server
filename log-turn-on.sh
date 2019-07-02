fead26bee950413e73a2eebc2ff76215920ba91e
gabyf <gaby.fulchic@ynov.com>
2019-07-02 10:54:51 +0200

    rights were bad ^^'

--- log-turn-on.sh
+++ log-turn-on.sh
@@ -3,14 +3,14 @@
 create_backupfile () {
     cd /home/easylia/turn-on-log
     touch "turn-on-log.$(date +%F_%R).log"
-    sudo chmod 777 "turn-on-log.$(date +%F_%R).log"
+    sudo chmod 755 "turn-on-log.$(date +%F_%R).log"
     echo "Le pc a démarré automatiquement le : $(date)" >> /home/easylia/turn-on-log/turn-on-log.$(date +%F_%R).log
 }
 
 #creating the folder and verifying if it already exists
 sudo mkdir /home/easylia/turn-on-log
 if [ $? -eq 0 ]; then
-    sudo chmod 777 /home/easylia/turn-on-log
+    sudo chmod 755 /home/easylia/turn-on-log
 fi
 
 #count number of files in the folder
